f4036282ce640fdd5fd32d3d4f46534efe25766c
Omar Sokolovic <omar.sokolovic@qssbh.com>
2020-08-13 16:10:05 +0200

    Add example of async method in repository

--- Clean Architecture/CleanArchitecture.Domain/IRepositories/IBookRepository.cs
+++ Clean Architecture/CleanArchitecture.Domain/IRepositories/IBookRepository.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArchitecture.Domain.IRepositories
 {
     public interface IBookRepository
     {
         IEnumerable<Book> GetBooks();
+        Task<IEnumerable<Book>> GetBooksAsync();
     }
 }
